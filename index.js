67340a65233a67f89a790b8e7532976bc3e45cbd
jthensley03 <jasonh@udel.edu>
2023-10-24 23:20:00 -0400

    working emailjs new template

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ emailjs.init('oI9cSzYQi-CbJD0OE');
 window.onload = function() {
     document.getElementById('contactForm').addEventListener('submit', function(event) {
         event.preventDefault();
-        emailjs.sendForm('service_o0joyin', 'template_3pgmocg', this)
+        emailjs.sendForm('service_o0joyin', 'template_zqsknev', this)
             .then(function() {
                     console.log('SUCCESS!');
                 }, function(error) {
@@ -27,3 +27,7 @@ for(var i = 1; i <= 12; i++){
 var minutesSelect = document.getElementById('minutes');
 minutesSelect.add(createOption(0, "00"));
 minutesSelect.add(createOption(30, "30"));
+
+var timeClassSelect = document.getElementById('time_class');
+timeClassSelect.add(createOption("AM", "AM"));
+timeClassSelect.add(createOption("PM", "PM"));
